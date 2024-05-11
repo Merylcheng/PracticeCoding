198d8046f1bc0db7bad9f5ee8ab3424a06ff1057
Merylcheng <163097111+Merylcheng@users.noreply.github.com>
2024-05-11 14:00:00 +0800

    new file

---
+++ add2.js
@@ -0,0 +1,18 @@
+// Challenge: 01-addOne
+// Difficulty: Basic
+// Prompt:
+// Write a function called addOne that takes a single number as an argument and returns that number plus 1.
+// Examples:
+// addOne(1) //=> 2
+// addOne(-5) //=> -4
+
+//always console.log to make it work first
+
+function addOne(num) {
+  console.log("working");
+  return num + 1;
+}
+
+addOne();
+console.log(addOne(1));
+console.log(addOne(-5));
